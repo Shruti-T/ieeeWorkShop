6cba47467a9d74b41273a046695a0d8260044142
Shruti Tiwari <sh03shruti@gmail.com>
2023-03-18 00:53:11 +0530

    twoTypes of forms registration

--- main.js
+++ main.js
@@ -1,22 +1,34 @@
-let eventForm;
 if (document.getElementById("formSubmit1")) {
-  eventForm = document.getElementById("formSubmit1");
+  document
+    .getElementById("formSubmit1")
+    .addEventListener("submit", (formresponse) => {
+      formresponse.preventDefault();
+      formData["name"] = document.getElementById("name").value;
+      formData["email"] = document.getElementById("email").value;
+      formData["event"] = "squabble";
+      sendRegistration(formData);
+      return false;
+    });
 } else if (document.getElementById("formSubmit2")) {
-  eventForm = document.getElementById("formSubmit2");
-}
+  document
+    .getElementById("formSubmit2")
+    .addEventListener("submit", (formresponse) => {
+      formresponse.preventDefault();
+      formData["name"] = document.getElementById("name").value;
+      formData["email"] = document.getElementById("email").value;
+      formData["name2"] = document.getElementById("name2").value;
+      formData["email2"] = document.getElementById("email2").value;
 
-eventForm.addEventListener("submit", (formresponse) => {
-  formresponse.preventDefault();
-  formData["name"] = document.getElementById("name").value;
-  formData["email"] = document.getElementById("email").value;
-  sendRegistration(formData);
-  return false;
-});
+      formData["event"] = "inquisitive";
+      sendRegistration(formData);
+      return false;
+    });
+}
 
 let formData = {};
 function sendRegistration() {
   $.ajax({
-    url: "https://script.google.com/macros/s/AKfycbzSRcVAyBlu6I4LJA4sw9cv8REuH_t_aOBFUWEwYQRQEOhJHIQCtjm_Xqxu0WsVfMF6eg/exec",
+    url: "https://script.google.com/macros/s/AKfycbxJgQYTmyW9LksxyaPvY8hMXKaLdFXIku9uydJ_HUkU6sVk24qEvumMJyJM9BWGYIOJ-Q/exec",
     type: "POST",
     data: formData,
 
